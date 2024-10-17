9eff72a72c51cccf02e61a7616c6d992c44431b1
im-sanny <raselparvezsanny@gmail.com>
2024-10-17 21:12:40 +0600

    update: updated the name of the forum and made some minor change

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ const latestPost = async () => {
   const response = await fetch(
     "https://openapi.programming-hero.com/api/retro-forum/latest-posts"
   );
+  
   const data = await response.json();
-
   const latestPosts = data;
-
   const latestContainer = document.getElementById("post-card");
 
   latestPosts.forEach((latestItem) => {
@@ -14,19 +13,29 @@ const latestPost = async () => {
     div.innerHTML = `
             <div class="card lg:h-[500px] bg-base-100 shadow-xl">
                 <figure class="px-5 pt-5">
-                    <img src="${latestItem.cover_image}" class="rounded-xl bg-[#12132D0D" />
+                    <img src="${
+                      latestItem.cover_image
+                    }" class="rounded-xl bg-[#12132D0D" />
                 </figure>
                 <div class="card-body ">
-                    <h2 class="card-title"><i class="fa-regular fa-calendar-check"></i> ${latestItem.author.posted_date}</h2>
+                    <h2 class="card-title"><i class="fa-regular fa-calendar-check"></i> ${
+                      latestItem.author.posted_date
+                    }</h2>
                     <p class="text-1xl font-bold">${latestItem.title}</p>
                     <p>${latestItem.description}</p>
                     <div class="card-actions">
                       <div>
-                        <img class="w-10 h-10 rounded-s-full" src="${ latestItem.profile_image}" alt="">
+                        <img class="w-10 h-10 rounded-s-full" src="${
+                          latestItem.profile_image
+                        }" alt="">
                         </div>
                         <div>
-                            <p class="text-1xl font-bold">${latestItem.author.name}</p>
-                            <p class="">${latestItem.author.designation || "Unknown"}</p>
+                            <p class="text-1xl font-bold">${
+                              latestItem.author.name
+                            }</p>
+                            <p class="">${
+                              latestItem.author.designation || "Unknown"
+                            }</p>
                         </div>
                     </div>
                 </div>
@@ -55,7 +64,9 @@ const allPost = async (searchText = "") => {
             <div class="hero-content px-5 lg:flex-row">
             
             <div class="avatar -mt-44 indicator">
-                    <span class="indicator-item badge ${items.isActive ? "bg-red-600" : "bg-green-400"}">
+                    <span class="indicator-item badge ${
+                      items.isActive ? "bg-red-600" : "bg-green-400"
+                    }">
                     </span>
                     <div class="w-10 h-10 rounded-lg">
                         <img alt="Tailwind CSS examples" src="${items.image}"/>
@@ -74,12 +85,22 @@ const allPost = async (searchText = "") => {
 
                         <div class="flex justify-between">
                             <div class="flex justify-between gap-10">
-                                <p class="lg:w-20"><i class="fa-regular fa-message"></i> ${items.comment_count}</p>
-                                <p class="lg:w-20"><i class="fa-regular fa-eye"></i> ${items.view_count}</p>
-                                <p class="lg:w-20"><i class="fa-regular fa-clock"></i> ${items.posted_time} min</p>
+                                <p class="lg:w-20"><i class="fa-regular fa-message"></i> ${
+                                  items.comment_count
+                                }</p>
+                                <p class="lg:w-20"><i class="fa-regular fa-eye"></i> ${
+                                  items.view_count
+                                }</p>
+                                <p class="lg:w-20"><i class="fa-regular fa-clock"></i> ${
+                                  items.posted_time
+                                } min</p>
                             </div>
                             <div class="-mt-1 w-2/6 lg:w-96 ml-5 lg:ml-36">
-                                <button onclick="handleAddInfo('${items.title}', '${items.view_count}')" class="btn bg-[#10B981] btn-sm btn-circle"><i class="fa-regular fa-envelope-open"></i></button>
+                                <button onclick="handleAddInfo('${
+                                  items.title
+                                }', '${
+      items.view_count
+    }')" class="btn bg-[#10B981] btn-sm btn-circle"><i class="fa-regular fa-envelope-open"></i></button>
                             </div>
                         </div>
                     </div>
